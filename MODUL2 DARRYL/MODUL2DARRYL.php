dd748bac8b1763fea2bf94971fa7d06387c01eac
darrylbastian <91937191+darrylbastian@users.noreply.github.com>
2021-10-24 23:56:56 +0700

    Modul2

---
+++ MODUL2 DARRYL/MODUL2DARRYL.php
@@ -0,0 +1,96 @@
+<?php 
+    // date_default_timezone_set("Asia/Jakarta");
+    // include "config.php";
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <title></title>
+  </head>
+  <style>
+      h4{text-align:center;}
+      .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: gray;
+        color: white;
+        text-align: center;
+        }
+  </style>
+  <body>
+    <!-- awal navbar -->
+    <!-- navbar menu -->
+     <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Booking</a>
+            </li>
+        </ul>
+        </nav>
+    <!-- akhir navbar -->
+    <div class="container">
+        <h4>WELCOME TO ESD VENUE RESERVATION
+        </h4>
+    </div>
+    <div>
+    <p class="text-center text-white bg-dark">Find your best deal for your event, here!</p>
+    </div>
+    
+    <div class="card-group">
+    <div class="card" style="width: 18rem;">
+    <img src="nusantara_hall.jpg" class="card-img-top" alt="nusantara hall">
+    <div class="card-body">
+    <h5 class="card-title">Nusantara Hall</h5>
+    <p class="card-text">$2000 / Hour <br> 5000 Capacity</p>
+    <p class="card-menu text-center">Free Parking <br> Full AC <br> Cleaning Service <br> Covid-19 Health Protocol</p>
+    <a href="#" class="btn btn-primary">Book now</a>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img src="garuda_hall.jpg" class="card-img-top" alt="garuda hall">
+    <div class="card-body">
+    <h5 class="card-title">Garuda Hall</h5>
+    <p class="card-text">$1000 / Hour <br> 2000 Capacity</p>
+    <p class="card-menu text-center">Free Parking <br> Full AC <br> NO Cleaning Service <br> Covid-19 Health Protocol</p>
+    <a href="#" class="btn btn-primary">Book now</a>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img src="gedung_serbaguna.jpg" class="card-img-top" alt="gedung serbaguna">
+    <div class="card-body">
+    <h5 class="card-title">Gedung Serba Guna</h5>
+    <p class="card-text">$500 / Hour <br> 500 Capacity</p>
+    <p class="card-menu text-center">NO Free Parking <br> NO Full AC <br> NO Cleaning Service <br> Covid-19 Health Protocol</p>
+    <a href="#" class="btn btn-primary">Book now</a>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="footer">
+  <p>Created by: darryl_1202183344</p>
+    </div>
+
+	
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-3.4.1.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
